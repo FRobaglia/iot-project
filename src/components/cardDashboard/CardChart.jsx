d70b69e12bcf7fd2622d010c72ae71c2db252de8
FRobaglia <felix.robaglia@gmail.com>
2021-07-21 22:01:05 +0200

    chore: add cardDashboard component files

---
+++ src/components/cardDashboard/CardChart.jsx
@@ -0,0 +1,11 @@
+/***** COMPONENTS *****/
+const CardChart = ({optimalMinValue, optimalMaxValue}) =>  {
+
+  return (
+    <div class="cardChart">
+      
+    </div>
+  ); 
+}
+
+export default CardChart;

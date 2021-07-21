922580108212759132b160bb839f1cf776bb73be
shatice <sarah.gungor@hetic.net>
2021-07-22 00:19:27 +0200

    chore: set onclick nav

--- src/components/nav/NavItem.jsx
+++ src/components/nav/NavItem.jsx
@@ -4,13 +4,20 @@ import { isBrowser } from 'react-device-detect';
 /***** COMPONENTS *****/
 import NavIcon from './NavIcon';
 import NavTitle from './NavTitle';
+ 
+const NavItem = ({ link, title, iconId, selected, onChange }) =>  {
 
-const NavItem = ({ link, title, iconId}) =>  {
+  function handleChange(title) {
+    onChange(title)
+  }
+
+  let selectedClass = (selected === title) ? " navItem--selected" : ""
+
   return (
-    <li className="navItem">
+    <li className={ "navItem" + selectedClass } onClick={() => { handleChange(title) }}>
       <Link to={ link } className="navItem__link">
         <NavIcon iconId={ iconId }></NavIcon>
-        { isBrowser ? <NavTitle title={ title }></NavTitle> : "" }
+        { isBrowser ? <NavTitle title={ title } ></NavTitle> : "" }
       </Link>
     </li>
   ); 
--- src/components/nav/index.jsx
+++ src/components/nav/index.jsx
@@ -1,15 +1,22 @@
+import React, { useState } from "react";
+
 /***** COMPONENTS *****/
 import NavItem from './NavItem';
 
 const Nav = () =>  {
+  const [selected, setSelected] = useState("Home")
+
+  function handleChange(selected) {
+    setSelected(selected)
+  }
 
   return (
     <nav className="nav">
       <ul className="navList">
-        <NavItem link="" title="Home" iconId="home"></NavItem>
-        <NavItem link="graphs" title="Graphs" iconId="graphs"></NavItem>
-        <NavItem link="settings" title="Settings" iconId="settings"></NavItem>
-        <NavItem link="help" title="Help" iconId="help"></NavItem>
+        <NavItem onChange={handleChange} selected={selected} link="" title="Home" iconId="home"></NavItem>
+        <NavItem onChange={handleChange} selected={selected} link="graphs" title="Graphs" iconId="graphs"></NavItem>
+        <NavItem onChange={handleChange} selected={selected} link="settings" title="Settings" iconId="settings"></NavItem>
+        <NavItem onChange={handleChange} selected={selected} link="help" title="Help" iconId="help"></NavItem>
       </ul>
     </nav>
   ); 

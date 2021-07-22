019ef39154fee083cde955a81d851ef630ef0ccc
FRobaglia <felix.robaglia@gmail.com>
2021-07-22 16:29:15 +0200

    chore: add foreach cards displaying

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -3,11 +3,44 @@ import Card from '../components/cardDashboard/index';
 
 const Home = () =>  {
 
+  const cards = [
+    {
+      title: "Température de l'air",
+      currentValue: 21,  
+      optimalMinValue: 15, 
+      optimalMaxValue: 27, 
+      dayMinValue: 17, 
+      dayMaxValue: 23,
+      sign: "°C",
+      off: false
+    },
+    {
+      title: "Humidité du sol",
+      currentValue: 38,  
+      optimalMinValue: 20, 
+      optimalMaxValue: 45, 
+      dayMinValue: 18, 
+      dayMaxValue: 30,
+      sign: "%",
+      off: true
+    },
+    {
+      title: "Luminosité",
+      currentValue: 38,  
+      optimalMinValue: 20, 
+      optimalMaxValue: 45, 
+      dayMinValue: 18, 
+      dayMaxValue: 30,
+      sign: "lx",
+      off: false
+    },
+  ]
+
   return (
     <main className="main main--home">
-      <Card title="Température de l'air" currentValue={21}  optimalMinValue={15} optimalMaxValue={27} dayMinValue={17} dayMaxValue={23} sign="°C" off={false} />
-      <Card title="Humidité du sol" currentValue={38}  optimalMinValue={20} optimalMaxValue={45} dayMinValue={18} dayMaxValue={30} sign="%" off={true} />
-      <Card title="Luminosité" currentValue={38}  optimalMinValue={20} optimalMaxValue={45} dayMinValue={18} dayMaxValue={30} sign="lx" off={false} />
+      {cards.map((card) => {       
+          return (<Card key={card.title} title={card.title} currentValue={card.currentValue} optimalMinValue={card.optimalMinValue} optimalMaxValue={card.optimalMaxValue} dayMinValue={card.dayMinValue} dayMaxValue={card.dayMaxValue} sign={card.sign} off={card.off} />) 
+      })}
     </main>
   ); 
 }

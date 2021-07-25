2e62ce9933800e812088d16789b9eeeafaa6872d
shatice <sarah.gungor@hetic.net>
2021-07-25 15:54:45 +0200

    chore: create datas.js

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,43 +1,9 @@
+import datas from '../datas'; 
+
 /***** COMPONENTS *****/
 import Card from '../components/cardDashboard/index';
 
 const Home = () =>  {
-
-  const cards = [
-    {
-      title: "Température de l'air",
-      currentValue: 21,  
-      optimalMinValue: 15, 
-      optimalMaxValue: 27, 
-      dayMinValue: 17, 
-      dayMaxValue: 23,
-      sign: "°C",
-      dropdown: false,
-      off: false
-    },
-    {
-      title: "Humidité du sol",
-      currentValue: 38,  
-      optimalMinValue: 20, 
-      optimalMaxValue: 45, 
-      dayMinValue: 18, 
-      dayMaxValue: 30,
-      sign: "%",
-      dropdown: false,
-      off: true
-    },
-    {
-      title: "Luminosité",
-      currentValue: 38,  
-      optimalMinValue: 20, 
-      optimalMaxValue: 45, 
-      dayMinValue: 18, 
-      dayMaxValue: 30,
-      sign: "lx",
-      dropdown: ["Capteur 1","Capteur 2","Capteur 3","Capteur 4","Capteur 5"],
-      off: false
-    },
-  ]
 
   return (
     <main className="main main--home">
@@ -45,8 +11,8 @@ const Home = () =>  {
         <h1>Tableau de bord</h1>
         <span><strong>Dernière actualisation :</strong> 19 Juillet 2021 - 14:45</span>
       </div>
-      {cards.map((card) => {       
-          return (<Card key={card.title} title={card.title} currentValue={card.currentValue} optimalMinValue={card.optimalMinValue} optimalMaxValue={card.optimalMaxValue} dayMinValue={card.dayMinValue} dayMaxValue={card.dayMaxValue} sign={card.sign} off={card.off} dropdown={card.dropdown} />) 
+      {datas.map((data) => {       
+          return (<Card key={data.title} title={data.title} currentValue={data.currentValue} optimalMinValue={data.optimalMinValue} optimalMaxValue={data.optimalMaxValue} dayMinValue={data.dayMinValue} dayMaxValue={data.dayMaxValue} sign={data.sign} off={data.off} dropdown={data.dropdown} />) 
       })}
     </main>
   ); 

21084659ef88554d03ae5f7d4921e8e38e9456b1
FRobaglia <felix.robaglia@gmail.com>
2021-07-25 17:46:27 +0200

    chore: make swiper more responsive

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,10 +1,13 @@
 import datas from '../datas'; 
 import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Pagination } from 'swiper/core';
+
 // Import Swiper styles
 import 'swiper/swiper.scss';
 
 /***** COMPONENTS *****/
 import Card from '../components/cardDashboard/index';
+SwiperCore.use([Pagination]);
 
 const Home = () =>  {
 
@@ -15,12 +18,33 @@ const Home = () =>  {
         <span><strong>Dernière actualisation :</strong> 19 Juillet 2021 - 14:45</span>
       </div>
       <Swiper
-      spaceBetween={30}
+      spaceBetween={20}
       slidesPerView={1}
-      centeredSlides={true}
+      pagination={true}
       onSlideChange={() => console.log('slide change')}
       onSwiper={(swiper) => console.log(swiper)}
       type="bullets"
+      breakpoints= {
+        {
+
+          // when window width is >= 320px
+          400: {
+            slidesPerView: 1.2,
+          },
+          650: {
+            slidesPerView: 2,
+          },
+
+          750: {
+            slidesPerView: 2.2,
+          },
+
+          950: {
+            slidesPerView: 3,
+          }
+
+        }
+      }
       >
         {datas.map((data) => {       
             return (
